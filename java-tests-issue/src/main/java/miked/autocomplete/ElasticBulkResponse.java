e96eb4e7121def687750207a3f2a20cff7f09065
Mike D <mdepouw@ceiamerica.com>
2020-02-24 15:50:52 -0500

    Autocomplete out of order | Elastic BulkResponse

---
+++ java-tests-issue/src/main/java/miked/autocomplete/ElasticBulkResponse.java
@@ -0,0 +1,12 @@
+package miked.autocomplete;
+
+import org.elasticsearch.action.bulk.BulkResponse;
+
+class ElasticBulkResponse {
+
+    void autoCompleteTest()
+    {
+        var bulkResponse = new BulkResponse(null, 1);
+        bulkResponse.
+    }
+}
