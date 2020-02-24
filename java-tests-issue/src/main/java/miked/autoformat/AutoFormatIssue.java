e25415a1cefae4f951e6e493ef14832c21cc1257
Michael DePouw <mdepouw@ceiamerica.com>
2020-02-24 15:49:22 -0500

    more autoformatting stuff (#12)

--- java-tests-issue/src/main/java/miked/autoformat/AutoFormatIssue.java
+++ java-tests-issue/src/main/java/miked/autoformat/AutoFormatIssue.java
@@ -5,23 +5,23 @@ import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 class AutoFormatIssue {
-    // @formatter:off
-    // before auto format
-    // https://stackoverflow.com/a/30026710/185123
-    static <T> CompletableFuture<List<T>> sequence(List<CompletableFuture<T>> com) {
-        return CompletableFuture.allOf(com.toArray(new CompletableFuture<?>[0]))
-                .thenApply(v -> com.stream()
-                        .map(CompletableFuture::join)
-                        .collect(Collectors.toList()));
-    }
-    // @formatter:on
+  // @formatter:off
+  // before auto format
+  // https://stackoverflow.com/a/30026710/185123
+  static <T> CompletableFuture<List<T>> sequence(List<CompletableFuture<T>> com) {
+    return CompletableFuture.allOf(com.toArray(new CompletableFuture<?>[0]))
+        .thenApply(v -> com.stream()
+            .map(CompletableFuture::join)
+            .collect(Collectors.toList()));
+  }
+  // @formatter:on
 
-    // after auto format
-    // https://stackoverflow.com/a/30026710/185123
-    static <T> CompletableFuture<List<T>> sequence2(List<CompletableFuture<T>> com) {
-        return CompletableFuture.allOf(com.toArray(new CompletableFuture<?>[0]))
-                .thenApply(v -> com.stream()
-                        .map(CompletableFuture::join)
-                        .collect(Collectors.toList()));
-    }
+  // after auto format
+  // https://stackoverflow.com/a/30026710/185123
+  static <T> CompletableFuture<List<T>> sequence2(List<CompletableFuture<T>> com) {
+    return CompletableFuture.allOf(com.toArray(new CompletableFuture<?>[0]))
+        .thenApply(v -> com.stream()
+            .map(CompletableFuture::join)
+            .collect(Collectors.toList()));
+  }
 }
