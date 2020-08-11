96a89db9aa984a16a9aeac1a809fbb0e2d68d51a
Michael DePouw <mdepouw@ceiamerica.com>
2020-08-11 13:26:16 -0400

    Xamarin ListView styles experimentation (#26)

---
+++ StyleInheritance/StyleInheritance/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using StyleInheritance.Services;
+using StyleInheritance.Views;
+
+namespace StyleInheritance
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            Device.SetFlags(new string[] { "AppTheme_Experimental" });
+
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
