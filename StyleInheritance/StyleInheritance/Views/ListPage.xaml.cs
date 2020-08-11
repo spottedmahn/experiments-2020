4c726564544c024553d71a67e94cfbc851110d91
Mike D <spottedmahn@gmail.com>
2020-08-11 13:24:22 -0400

    Xamarin ListView styles experimentation

---
+++ StyleInheritance/StyleInheritance/Views/ListPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StyleInheritance.Views
+{
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new[] { "a", "b", "c" };
+        }
+    }
+}
