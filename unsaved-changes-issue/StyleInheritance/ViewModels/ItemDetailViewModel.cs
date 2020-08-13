7a71135a8a10173558a905c295375521472d6022
Michael DePouw <mdepouw@ceiamerica.com>
2020-08-12 22:00:15 -0400

    add unsaved issue (#28)

---
+++ unsaved-changes-issue/StyleInheritance/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using StyleInheritance.Models;
+
+namespace StyleInheritance.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
