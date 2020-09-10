4f7ea0bc50945aae5ccd1123bff22b60b5b76ad5
Mike DePouw <mdepouw@ceiamerica.com>
2020-09-10 13:39:33 -0400

    Fetch research

---
+++ fetch-research/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> AsyncGood(int seconds)
+        {
+            await Task.Delay(1000 * seconds);
+
+            return Json(new { Hello = $"World Good - {seconds}" });
+        }
+
+        public IActionResult AsyncBad(int seconds)
+        {
+            Task.Delay(1000 * seconds).GetAwaiter().GetResult();
+
+            return Json(new { Hello = $"World Bad - {seconds}" });
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ fetch-research/WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+fetch('./home/AsyncGood?seconds=6')
+    .then(function (response) {
+        handleResponse(response);
+    });
+
+for (var i = 0; i < 200; i++) {
+    fetch('./home/AsyncGood?seconds=1')
+        .then(function (response) {
+            handleResponse(response);
+        });
+}
+
+fetch('./home/AsyncBad?seconds=9')
+    .then(function (response) {
+        handleResponse(response);
+    });
+
+fetch('./home/AsyncBad?seconds=2')
+    .then(function (response) {
+        handleResponse(response);
+    });
+
+function handleResponse(response, msg) {
+    response.json().then(function (data) {
+        console.info(`got async response (${msg}) ${JSON.stringify(data)}`);
+    });
+}
