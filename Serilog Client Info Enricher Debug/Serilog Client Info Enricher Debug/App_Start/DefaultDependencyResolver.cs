89d9a0d73072750a64a6271c438069c4dbe6e207
Michael DePouw <spottedmahn@gmail.com>
2020-12-12 22:08:55 -0500

    add repro for MissingMethodException Serilog.Context.LogContext.Push (#32)
    * add repro for MissingMethodException Serilog.Context.LogContext.Push
    
    * update readme

---
+++ Serilog Client Info Enricher Debug/Serilog Client Info Enricher Debug/App_Start/DefaultDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+
+namespace Serilog_Client_Info_Enricher_Debug
+{
+    public class DefaultDependencyResolver : IDependencyResolver, System.Web.Mvc.IDependencyResolver
+    {
+        private readonly IServiceProvider provider;
+
+        public DefaultDependencyResolver(IServiceProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return provider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return provider.GetServices(serviceType);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Serilog Client Info Enricher Debug/readme.md
@@ -0,0 +1,7 @@
+https://stackoverflow.com/questions/65268938/missingmethodexception-serilog-context-logcontext-push
+
+https://github.com/mo-esmp/serilog-enrichers-clientinfo/issues/6
+
+![Exception](readme-resources/exception.png)
+
+![Modules](readme-resources/modules.png)
