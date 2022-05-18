a2a04641f92535444966049259fd4c74ec7dab35
Michael DePouw <spottedmahn@gmail.com>
2022-05-18 11:02:32 -0400

    dapr multi-container guide
    https://docs.microsoft.com/en-us/dotnet/architecture/dapr-for-net-developers/getting-started#build-a-multi-container-dapr-application

---
+++ DaprMultiContainer/MyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly DaprClient daprClient;
+
+        public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            this.daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forcasts = await daprClient.InvokeMethodAsync<
+                IEnumerable<WeatherForecast>>(HttpMethod.Get,
+                "MyBackEnd",
+                "weatherforecast");
+            ViewData["WeatherForecastData"] = forcasts;
+        }
+    }
+}
---
+++ DaprMultiContainer/MyFrontEnd/WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFrontEnd;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF { get; set; }
+
+    public string Summary { get; set; } = string.Empty;
+}
