f5787ac9ca97b4f5ccc5a97c95b1974aff21dc01
Mike DePouw <mdepouw@ceiamerica.com>
2022-10-03 19:15:20 -0400

    write some of my own tests based upon my learnings

---
+++ Dapr unit testing research/Dapr unit test prj 1/ExampleService.cs
@@ -0,0 +1,21 @@
+﻿using Dapr.Client;
+
+namespace DaprUnitTestPrj1
+{
+    internal class ExampleService
+    {
+        private DaprClient daprClient;
+
+        public ExampleService(DaprClient daprClient)
+        {
+            this.daprClient = daprClient;
+        }
+
+        internal async Task<bool> IsTheCustomerNameMikeD()
+        {
+            var customer = await daprClient.InvokeMethodAsync<Customer>(HttpMethod.Get,
+                "my-cool-app", "customer");
+            return customer?.Name == "Mike D.";
+        }
+    }
+}
