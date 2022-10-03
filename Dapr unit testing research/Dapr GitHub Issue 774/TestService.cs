453cff20f1c07a17f44fb20c8a255ee3154b92b0
Mike DePouw <mdepouw@ceiamerica.com>
2022-10-03 13:56:00 -0400

    create full runnable test from 774 partial solution

---
+++ Dapr unit testing research/Dapr GitHub Issue 774/TestService.cs
@@ -0,0 +1,19 @@
+﻿using Dapr.Client;
+
+namespace DaprGitHubIssue774
+{
+    internal class TestService
+    {
+        private readonly DaprClient daprClient;
+
+        public TestService(DaprClient daprClient)
+        {
+            this.daprClient = daprClient;
+        }
+
+        internal async Task<string> TestClientAsync(string v)
+        {
+            return await daprClient.InvokeMethodAsync<string>(HttpMethod.Get, "my-cool-app", "my-endpoint");
+        }
+    }
+}
