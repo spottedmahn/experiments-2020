740d67acd90811d567731a22d84d4ecae48f0a3e
Mike DePouw <mdepouw@ceiamerica.com>
2022-10-04 09:15:30 -0400

    Example of how to mock DaprClient.GetBulkSecretAsync - 68362431

---
+++ Dapr unit testing research/Bulk secret unit testing/ExampleService.cs
@@ -0,0 +1,19 @@
+﻿using Dapr.Client;
+
+namespace Bulk_secret_unit_testing
+{
+    internal class ExampleService
+    {
+        private DaprClient daprClient;
+
+        public ExampleService(DaprClient daprClient)
+        {
+            this.daprClient = daprClient;
+        }
+
+        internal async Task<Dictionary<string, Dictionary<string, string>>> GetBulkSecrets(string storeName)
+        {
+            return await daprClient.GetBulkSecretAsync(storeName);
+        }
+    }
+}
