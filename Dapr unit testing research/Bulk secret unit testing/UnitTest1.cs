03c5329f828a0144c167960150822c397132994e
Michael DePouw <spottedmahn@gmail.com>
2022-10-04 09:28:50 -0400

    Example of how to mock DaprClient.GetBulkSecretAsync - 68362431 (#46)
    * Example of how to mock DaprClient.GetBulkSecretAsync - 68362431
    
    * add primitive readme
    
    * clean namespace name

---
+++ Dapr unit testing research/Bulk secret unit testing/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Dapr.Client;
+using FluentAssertions;
+using Moq;
+
+namespace BulkSecretUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// potential answer for "how to mock GetBulkSecretAsync"
+        /// https://stackoverflow.com/questions/68362431/how-do-you-do-unit-testing-with-dapr
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod("How to mock GetBulkSecretAsync - 68362431")]
+        public async Task TestMethod1()
+        {
+            //arrange
+            var daprClient = new Mock<DaprClient>();
+            var exampleService = new ExampleService(daprClient.Object);
+
+            daprClient.Setup(d => d.GetBulkSecretAsync("my-store",
+                It.IsAny<IReadOnlyDictionary<string, string>>(),
+                It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Dictionary<string, Dictionary<string, string>>
+                {
+                    {
+                        "example",
+                        new Dictionary<string, string>
+                        {
+                            { "i don't understand the builk API (yet)", "some value" }
+                        }
+                    }
+                });
+
+            //act
+            var actual = await exampleService.GetBulkSecrets("my-store");
+
+            //assert
+            actual.Should().BeEquivalentTo(new Dictionary<string, Dictionary<string, string>>
+            {
+                {
+                    "example",
+                    new Dictionary<string, string>
+                    {
+                        { "i don't understand the builk API (yet)", "some value" }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Dapr unit testing research/Bulk secret unit testing/readme.md
@@ -0,0 +1,45 @@
+﻿https://stackoverflow.com/questions/68362431
+
+>How do you do unit testing with Dapr?  
+in this particular instance, I'm using the 
+DaprClient.GetBulkSecretAsync method but I'd like
+a solution that I can use elsewhere, if possible.
+
+```csharp
+[TestMethod("How to mock GetBulkSecretAsync - 68362431")]
+public async Task TestMethod1()
+{
+	//arrange
+	var daprClient = new Mock<DaprClient>();
+	var exampleService = new ExampleService(daprClient.Object);
+
+	daprClient.Setup(d => d.GetBulkSecretAsync("my-store",
+		It.IsAny<IReadOnlyDictionary<string, string>>(),
+		It.IsAny<CancellationToken>()))
+		.ReturnsAsync(new Dictionary<string, Dictionary<string, string>>
+		{
+			{
+				"example",
+				new Dictionary<string, string>
+				{
+					{ "i don't understand the builk API (yet)", "some value" }
+				}
+			}
+		});
+
+	//act
+	var actual = await exampleService.GetBulkSecrets("my-store");
+
+	//assert
+	actual.Should().BeEquivalentTo(new Dictionary<string, Dictionary<string, string>>
+	{
+		{
+			"example",
+			new Dictionary<string, string>
+			{
+				{ "i don't understand the builk API (yet)", "some value" }
+			}
+		}
+	});
+}
+```
